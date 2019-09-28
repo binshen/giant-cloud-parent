edfb378f5765c419474abec89fb864efa28518ab
沈斌 <bin.shen@ailingua.com.cn>
2019-09-28 21:49:35 +0800

    添加更多的service-module

---
+++ giant-cloud-service/giant-service-bike/src/main/java/com/giant/cloud/service/BikeServiceApplication.java
@@ -0,0 +1,18 @@
+package com.giant.cloud.service;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@MapperScan("com.giant.cloud.service.mapper")
+@ServletComponentScan("com.giant.cloud.service.filter")
+@SpringBootApplication
+public class BikeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeServiceApplication.class, args);
+    }
+}
