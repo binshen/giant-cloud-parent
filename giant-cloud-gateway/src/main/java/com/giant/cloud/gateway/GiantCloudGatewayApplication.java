7fc0a8ec7be3729feb3156e1eac701bcadefd47d
沈斌 <bin.shen@ailingua.com.cn>
2019-09-30 13:42:48 +0800

    springcloud gateway网关熔断降级

--- giant-cloud-gateway/src/main/java/com/giant/cloud/gateway/GiantCloudGatewayApplication.java
+++ giant-cloud-gateway/src/main/java/com/giant/cloud/gateway/GiantCloudGatewayApplication.java
@@ -9,9 +9,4 @@ public class GiantCloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GiantCloudGatewayApplication.class, args);
     }
-
-//    @RequestMapping("/fallback_qrcode")
-//    public Mono<String> fallback_qrcode() {
-//        return Mono.just("fallback_qrcode");
-//    }
 }
